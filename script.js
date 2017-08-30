7c54a42c2da0ab6f8e51fa80c6738abd32e061ae
Nestor Gomez Jimenez <gomez315@umn.edu>
2017-08-30 01:28:53 -0500

    edabit problem

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("testing this out!");
+console.log("testing this out!!!!!");
+//solving an Edabit Problem: Create a function that 
+//takes an array of numbers and returns the smallest number 
+//in the set.
+function findSmallestNum(arr) {   
+   arr = [3,2,5,4];
+   arr = arr.sort();
+    if (arr[0]<arr[1]){
+        return arr[0];
+    }
+    else {
+        return arr;
+    }    
+  }
+//   findSmallestNum.sort();
+ console.log(findSmallestNum());
